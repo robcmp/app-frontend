430be39dee209a00f189418ceceb7175e4cfa057
Roberto Mendoza <roberto.cmp16@gmail.com>
2023-01-07 14:15:33 -0300

    added PrivateRoutes to handle access in App

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,8 @@ import Home from "../views/Home";
 import Login from "../views/Login";
 import Navbar from "./Navbar";
 import NotFound from "../views/NotFound";
+import PrivateRoutes from "./PrivateRoutes";
+import Task from "../views/Task";
 
 const Layout = () => {
   return (
@@ -14,6 +16,12 @@ const Layout = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         {/*End Public routes in the App */}
+
+        {/* Routes for Auth Users */}
+        <Route element={<PrivateRoutes />}>
+          <Route path="/task" element={<Task />} />
+        </Route>
+        {/* End Routes for Auth Users */}
 
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
