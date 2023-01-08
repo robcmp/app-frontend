ff094f7da3748e2f14af19a13b48fed31dd32b7e
Roberto Mendoza <roberto.cmp16@gmail.com>
2023-01-08 15:11:02 -0300

    added style sheet for home view

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,44 +4,47 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import "../styles/home.css";
 
 const Home = () => {
   return (
-    <Box
-      sx={{
-        bgcolor: "background.paper",
-        pt: 8,
-        pb: 6,
-      }}
-    >
-      <Container maxWidth="sm">
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="text.primary"
-          gutterBottom
-        >
-          Lista de Tareas App
-        </Typography>
-        <Typography
-          variant="h5"
-          align="center"
-          color="text.secondary"
-          paragraph
-        >
-          Texto de bievenida :B
-        </Typography>
-        <Stack
-          sx={{ pt: 4 }}
-          direction="row"
-          spacing={2}
-          justifyContent="center"
-        >
-          <Button variant="contained">Acceder</Button>
-        </Stack>
-      </Container>
-    </Box>
+    <>
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Lista de Tareas App
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            Texto de bievenida :B
+          </Typography>
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <Button variant="contained">Acceder</Button>
+          </Stack>
+        </Container>
+      </Box>
+    </>
   );
 };
 
